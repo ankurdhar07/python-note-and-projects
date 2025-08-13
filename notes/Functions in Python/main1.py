f67d4b8520503d02ab3302d8d267028641efb967
Ankur <ankurdhar23@gmail.com>
2025-08-14 02:47:50 +0530

    Updated notes

---
+++ notes/Functions in Python/main1.py
@@ -0,0 +1,53 @@
+# a = 2
+# b = 4
+# c = 8
+# d = 6
+# gmean = (a * b)/(a + b)
+# print(gmean)
+# gmean = (c * d)/(c + d)
+# print(gmean)
+
+# a = 2
+# b = 4
+# c = 8
+# d = 6
+# def calculateGmean (a , b):
+#     mean = (a * b)/(a + b)
+#     print(mean)
+# calculateGmean(a, b)
+# calculateGmean(c, d)    
+
+# def calculateGmean(a, b):
+#     mean = (a * b)/(a + b)
+#     print(mean)
+# a = 4
+# b = 3
+# if (a > b ):
+#     print("\nFirst number is greater than second number\n")
+# else:
+#     print("\nFirst number is not greater than second number or equal\n")    
+# calculateGmean(a, b)
+
+# c = 5
+# d = 6
+# if (c > d):
+#     print("First number is greater than second number")
+# else:
+#     print("First number is not greater than second number")
+# calculateGmean(c, d)
+
+# def isGreater(a, b):
+#     if (a > b ):
+#         print("\nFirst number is greater than second number\n")
+#     else:
+#         print("\nFirst numeber is not greater than second number or equal\n")    
+# a = 6
+# b = 3
+# isGreater(a, b)
+
+def isLesser(a, b):
+    pass
+def name(Sam, Ram):
+    print("Hello", Sam, Ram)
+
+name("Rahul", "Sonu")
---
+++ notes/Functions in Python/main2.py
@@ -0,0 +1,50 @@
+# def isGreater(a, b):
+#     if (a > b):
+#         print("\nFirst number is greater than the second number\n")
+#     else:
+#         print("\nFirst number is not greater than the second number\n")
+# a = 5
+# b = 3
+# isGreater(a, b)
+
+# def isLesser(c, d):
+#     if (c > d):
+#         print("First number is greater than the second number")
+#     else:
+#         print("First number is not greater than the second number")
+# c = 5
+# d = 3
+# isLesser(c, d)
+
+# def isLesser(x, z):
+    # pass
+
+# def calculateGmean(f, h):
+#     mean = (f * h)/(f + h)
+#     print(mean)
+# f = 3
+# h = 6
+# v = 4
+# m = 3
+# calculateGmean(f, h)
+# calculateGmean(v, m)
+
+# t = 6
+# k = 2
+# def sum(t, k):
+#     sum = (t + k)
+#     print(sum)
+# sum(t, k)
+# a = 4
+# b = 6
+# def multiplication(a, b):
+#     result = (a * b)
+#     print(result)
+# multiplication(a, b)
+
+def calculategmean(f, g):
+    mean = (f * g)/(f + g)
+    print(mean)
+f = 4
+g = 5
+calculategmean(f, g)
